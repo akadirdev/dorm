ed5a513d8592114571b9990200ed1b87491f4dc8
akadirdev <akadir.dd@gmail.com>
2022-11-01 13:29:53 +0300

    export modules

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@
 export * from "./dorm";
 export * from "./definitions";
 export * from "./decorators";
+export * from "./connectors";
+export * from "./filters";
+export * from "./schemas/model.schema";
+export * from "./entity-repository";
+export * from "./repository";
 
 // basicUsage();
